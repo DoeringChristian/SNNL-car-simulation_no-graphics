d0a1251494d3dcd9235d8252f860dcabfba6604d
DoeringChristian <doering.christian.2001@gmx.de>
2018-01-25 21:49:09 +0100

    changed saving methode

--- main.cpp
+++ main.cpp
@@ -35,14 +35,19 @@ int main(){
         n2.update();
         
         c.setRotspeed((n2.getOutput()[0]-0.5));
-        c.setSpeed(n2.getOutput()[1]-0.5);
+        c.setSpeed(n2.getOutput()[1]);
         
-        cout <<  n2.getOutput()[0]-0.5 << "|" << n2.getOutput()[1]-0.5 << "|" << generation << "|" << tr.currentNet << "|" << fTC << "|" << c.getPosition().x << endl;        
+        cout <<  n2.getOutput()[0]-0.5 << "|" << n2.getOutput()[1] << "|" << generation << "|" << tr.currentNet << "|" << fTC << "|" << c.getPosition().x << endl;        
         if(c.isColliding() || fTC > 10000 || (fTC > 1000 && c.getPosition().x < 60)){
             xq /= fTC;
-            if(tr.currentNet == 0)
+            if(tr.currentNet == 0){
                 generation++;
-            n2 = tr.update(-(c.getPosition().x-xq/fTC),0.3,0.1);//-((pow(c.getPosition().x,2)*0.01)/(pow(c.getPosition().x,2)*10)+0.01));//0.1/(c.getPosition().x-xq/fTC));//0.001);
+                ofstream log;
+                log.open("log.txt",ofstream::out | ofstream::app);
+                log << "position: " << c.getPosition().x << " generation: " << generation << endl;
+                log.close();
+            }
+            n2 = tr.update(-(c.getPosition().x-xq/fTC),0.1,0.1);//-((pow(c.getPosition().x,2)*0.01)/(pow(c.getPosition().x,2)*10)+0.01));//0.1/(c.getPosition().x-xq/fTC));//0.001);
             c.setPosition(vector2d(50,50));
             c.setRotation(1.5);
             xq = 50;
